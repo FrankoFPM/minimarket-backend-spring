bb23746c2ef3228d232a40f3974cb3358c5700b0
FrankoFPM <frank.13.gp@gmail.com>
2025-04-27 02:54:36 -0500

    Start project

---
+++ src/main/java/org/minimarket/minimarketbackendspring/MinimarketBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package org.minimarket.minimarketbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimarketBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimarketBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/minimarket/minimarketbackendspring/MinimarketBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.minimarket.minimarketbackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimarketBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
