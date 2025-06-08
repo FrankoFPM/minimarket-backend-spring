256756fac01406ace566ea8f20b8b06c65da18be
FrankoFPM <frank.13.gp@gmail.com>
2025-06-08 00:30:32 -0500

    Cambia los atributos de ProveedorDTO a no finales para permitir la modificación de sus valores

--- src/main/java/org/minimarket/minimarketbackendspring/dtos/ProveedorDTO.java
+++ src/main/java/org/minimarket/minimarketbackendspring/dtos/ProveedorDTO.java
@@ -9,15 +9,15 @@ import java.time.OffsetDateTime;
  * DTO para la entidad Proveedor.
  */
 public class ProveedorDTO {
-    private final Long id;
-    private final String nombre;
-    private final String contacto;
-    private final String telefono;
-    private final String direccion;
-    private final String email;
-    private final String estado;
-    private final OffsetDateTime createdAt;
-    private final OffsetDateTime updatedAt;
+    private Long id;
+    private String nombre;
+    private String contacto;
+    private String telefono;
+    private String direccion;
+    private String email;
+    private String estado;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
 
     public ProveedorDTO(Long id, String nombre, String contacto, String telefono, String direccion, String email, String estado, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
         this.id = id;
@@ -35,36 +35,72 @@ public class ProveedorDTO {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getContacto() {
         return contacto;
     }
 
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
     public String getTelefono() {
         return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 
     public String getDireccion() {
         return direccion;
     }
 
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getEstado() {
         return estado;
     }
 
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
     public OffsetDateTime getCreatedAt() {
         return createdAt;
     }
 
+    public void setCreatedAt(OffsetDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     public OffsetDateTime getUpdatedAt() {
         return updatedAt;
+    }
+
+    public void setUpdatedAt(OffsetDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 
     @Override
